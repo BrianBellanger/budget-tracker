e96a198541c9476f1aeabe2e87851deae7bab833
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-17 22:52:36 -0500

    RemovedWebpackPackageJSON

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connection.on("connected", () =>
+//   console.log("Connected to MongoDB Endpoint")
+// );
+
+// mongoose.connection.on("error", (err) =>
+//   console.log(`Mongoose default connection error: ${err}`)
+// );
+
 // routes
 app.use(require("./routes/api.js"));
 
